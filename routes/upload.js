3ba17a28e0b11955248870bc4d5743449f75e565
xianbo.yu <yxbneil@163.com>
2016-11-10 19:51:54 +0800

    for linux version

--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,27 @@ var express = require('express');
 var fs = require('fs');
 var exec = require('child_process').exec;
 var multiparty = require('multiparty');
+var cmdStr = "bash /home/sjtuicat/hansneil/GFP-DCN/GFP-DCN-Code/code/codes/GFPDCN/mrun.sh upload.mp4 1";
+
+function checkFinish(callback) {
+    if (fs.existsSync('/home/sjtuicat/hansneil/GFP-DCN/GFP-DCN-Code/code/codes/GFPDCN/finish.txt')) {
+        data = fs.readFileSync('/home/sjtuicat/hansneil/GFP-DCN/GFP-DCN-Code/code/codes/GFPDCN/finish.txt', 'utf-8');
+        if (data == 'Finish') {
+            console.log('Finished');
+            callback();
+        } else {
+            setTimeout(function(){
+                console.log("Not Yet");
+                checkFinish(callback);
+              }, 1000);
+        }
+    } else {
+        setTimeout(function(){
+            console.log("Not Yet");
+            checkFinish(callback);
+        }, 1000);
+    }
+}
 
 exports.upload = function(req, res) {
     var form = new multiparty.Form();
@@ -13,7 +34,9 @@ exports.upload = function(req, res) {
     form.parse(req, function(err, fields, files){
         var uploads = files.uploads;
         fs.renameSync(uploads[0].path, form.uploadDir + uploads[0].originalFilename);
+        exec(cmdStr);
+        checkFinish(function () {
+          res.send(200, {result: 100});
+        });
     });
-
-    res.send(200, {result: 100});
 };
